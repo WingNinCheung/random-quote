8563d5421458f23d5bb479765b1d104dd4f29b8f
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2023-09-30 17:19:13 -0700

    completed basic requirement: get new quote and change bg color after clicking

---
+++ js/helper.js
@@ -0,0 +1,21 @@
+export async function changeBackgroundColor() {
+  const wipeAnimation = document.getElementById("wipe-animation");
+  const randomColor = getRandomColor();
+
+  document.documentElement.style.setProperty("--new", randomColor);
+  wipeAnimation.style.backgroundPosition = "left top";
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  document.documentElement.style.setProperty("--curr", randomColor);
+  wipeAnimation.style.backgroundPosition = "right bottom";
+  return new Promise((resolve) => setTimeout(resolve, 500));
+}
+
+export function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
--- js/main.js
+++ js/main.js
@@ -3,18 +3,57 @@
  * http://safetybelt.pythonanywhere.com/quotes/random
  */
 
+import { changeBackgroundColor } from "./helper.js";
 
-function generate() {
-    const elem = document.getElementById('quote');
-    elem.innerHTML = "Quote goes here!";
+document.addEventListener("DOMContentLoaded", function () {
+  document.documentElement.style.setProperty("--new", "red");
+  document.documentElement.style.setProperty("--curr", "lightblue");
+  getQuote();
+});
+
+function handleCustomError(error) {
+  generate(null, error);
 }
 
-(function() {
-    const button = document.getElementById('generate');
-    if (button) {
-        button.addEventListener('click', (e) => {
-            e.preventDefault();
-            generate();
-        });
+// Get quote from the API endpoint
+async function getQuote() {
+  const API_ENDPOINT = "http://safetybelt.pythonanywhere.com/quotes/random";
+  try {
+    const response = await fetch(API_ENDPOINT);
+    if (response.ok) {
+      const data = await response.json();
+      generate(data);
+    } else {
+      handleCustomError(response.status);
     }
+  } catch (error) {
+    handleCustomError(error);
+  }
+}
+
+// Receive the quote info and display it in the frontend
+function generate(data, error) {
+  const quote = document.getElementById("quote");
+  const author = document.getElementById("author");
+  if (data) {
+    quote.innerHTML = data.quote;
+    author.innerHTML = `- ${data.author}`;
+  } else {
+    quote.innerHTML = `"Oops It's a ${error} error. Something went wrong. It's not you. It's us. Please try again!"`;
+    author.innerHTML = "- Ricky Cheung";
+  }
+}
+
+// Get a new quote and change bg color when the button is clicked
+(function () {
+  const button = document.getElementById("generate");
+  if (button) {
+    button.addEventListener("click", async (e) => {
+      e.preventDefault();
+      getQuote();
+      button.disabled = true;
+      await changeBackgroundColor();
+      button.disabled = false;
+    });
+  }
 })();
